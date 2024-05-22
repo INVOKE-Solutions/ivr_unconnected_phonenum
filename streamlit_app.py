fee98c7d26c1754adfa1bd51045bfdfaab95e6ec
judmn <junaidi@invokeisdata.com>
2024-05-22 23:01:20 +0800

    fixed rerun issue with st.experimental_fragment decorator

---
+++ streamlit_app.py
@@ -0,0 +1,130 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+from datetime import datetime
+from utils import IvrProcessor
+
+st.set_page_config(
+    page_title='IVR Unconnected Phone Numbers',
+    # page_icon=Image.open('./images/invoke_logo.png'),
+    layout="centered",    
+    initial_sidebar_state="auto"
+)
+
+st.title('IVR Unconnected Phone Numbers')
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            header {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+st.markdown(
+    '''
+    **NOTE:** This web app is meant to be used when all phone numbers for a given population is included in the sampling.
+    '''
+)
+
+def main():
+    
+    # Check if data is already processed and available in session state
+    if 'processed' not in st.session_state:
+        st.session_state['processed'] = False
+      
+    if 'all_unconnected_phonenum' not in st.session_state:
+
+        # Setting up the file uploader widget
+        uploaded_files = st.file_uploader("Upload IVR Files (.csv format)",
+                                          accept_multiple_files=True, 
+                                          type=['csv', 'xlsx'], 
+                                          help='You can upload multiple csv files at once')
+
+        # Display the count of uploaded files
+        if uploaded_files:
+            st.write(f"Number of files uploaded: {len(uploaded_files)}")
+
+            if st.button('Process'):
+                with st.spinner("Processing the files..."):
+
+                    unconnected_phonenums = []
+                    total_cr = 0
+
+                    # Extract unconnected phone numbers only
+                    for uploaded_file in uploaded_files:
+                        
+                        # Initialize class
+                        ip = IvrProcessor()
+
+                        ivr_file = ip.read_file(uploaded_file)
+
+                        unconnected_phonenum = ip.extract_unconnected_phonenum(ivr_file)
+
+                        unconnected_phonenums.append(unconnected_phonenum)
+
+                        cr_count = ip.calculate_total_cr(ivr_file)
+
+                        total_cr += cr_count
+
+                    all_unconnected_phonenum = pd.concat(unconnected_phonenums, ignore_index=True).drop_duplicates()
+
+                    st.session_state['all_unconnected_phonenum'] = all_unconnected_phonenum
+                    
+                    st.session_state['processed'] = True
+
+                    # Calculate total CR
+
+
+    if st.session_state['processed']:
+        st.success("Files have been processed successfully ✨")
+
+    if st.session_state['processed']:
+
+        # Display a snippet of the cleaned data
+        st.markdown("### Data Preview")
+
+        st.write(f"Total CR: {total_cr:,}")
+
+        st.write(f"Total unconnected phone numbers: {len(st.session_state['all_unconnected_phonenum']):,}")
+
+        st.dataframe(st.session_state['all_unconnected_phonenum'].sample(6))
+
+        # Current date for the filename
+        formatted_date = datetime.now().strftime("%d%m%Y")
+
+        # Format the default filename
+        default_filename = f'IVR_unconnected_phonenum_v{formatted_date}.csv'
+
+        # Use the default filename in the text input, allowing the user to edit it
+        output_filename = st.text_input("Custom the file name before downloading if needed", value=default_filename)
+
+        # Check if the output filename ends with '.csv', if not append '.csv'
+        if not output_filename.lower().endswith('.csv'):
+            output_filename += '.csv'
+    
+        # Download unconnected phone numbers list
+        if st.session_state['processed']:
+            
+            # Randomize the phone numbers
+            all_unconnected_phonenum = st.session_state['all_unconnected_phonenum'].sample(len(st.session_state['all_unconnected_phonenum']))
+
+            # Convert to csv
+            data_as_csv = all_unconnected_phonenum.to_csv(index=False).encode('utf-8')
+
+            st.download_button(
+                label="Download file",
+                data=data_as_csv,
+                file_name=output_filename,
+                mime='text/csv',
+                key='download'
+            )
+        
+        if 'downloaded' in st.session_state:
+            st.write('**\*Reload the page to upload another IVR results.**')
+        else:
+            st.session_state['downloaded'] = True
+        
+if __name__ == '__main__':
+    main()
--- utils.py
+++ utils.py
@@ -1,61 +1,62 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 
-def merger(df_list, phonenum_list):
-    """
-    Concatenates lists of DataFrames and renames a column.
+class IvrProcessor:
+    def __init__(self):
+        pass
 
-    Parameters:
-    - df_list (list of pd.DataFrame): List of DataFrames to be concatenated vertically.
-    - phonenum_list (list of pd.DataFrame): List of phone number DataFrames to be concatenated vertically.
+    def read_file(self, uploaded_file) -> pd.DataFrame:
+        
+        df = pd.read_csv(uploaded_file, skiprows=1, names=range(100), engine='python')
 
-    Returns:
-    - df_merge (pd.DataFrame): Concatenated DataFrame of df_list.
-    - phonenum_combined (pd.DataFrame): Concatenated DataFrame of phonenum_list with 'PhoneNo' column renamed to 'phonenum'.
-    """
-    # Check if the lists are not empty before concatenating
-    if df_list:
-        df_merge = pd.concat(df_list, axis='index')
-    else:
-        df_merge = pd.DataFrame()  # Return an empty DataFrame if list is empty
+        # Drop empty columns
+        df.dropna(axis='columns', how='all', inplace=True)
 
-    if phonenum_list:
-        phonenum_combined = pd.concat(phonenum_list, axis='rows')
-        phonenum_combined.rename(columns={'PhoneNo': 'phonenum'}, inplace=True)
-    else:
-        phonenum_combined = pd.DataFrame()  # Return an empty DataFrame if list is empty
+        # Assign the first row as the column names
+        df.columns = df.iloc[0]
 
-    return df_merge, phonenum_combined
+        # Drop the original row that had been assigned as column names
+        df = df.iloc[1:]
+
+        return df
 
 
+    def extract_unconnected_phonenum(self, df) -> pd.DataFrame:
+        """
+        Process the uploaded IVR raw results to extract unconnected phone numbers. In other words, we want phone numbers that 
+        didn't participate in the IVR survey yet.
 
-def extract_unconnected_phonenum(uploaded_file):
-    """
-    Process the uploaded CSV files to extract unconnected phone numbers. In other words, we want phone numbers that 
-    didn't participate in the IVR survey yet.
+        Parameters:
+        uploaded_file: A raw IVR result downloaded from the ARIA system.
 
-    Parameters:
-    - uploaded_file: A file-like object representing the uploaded CSV file.
-                     This object must support file-like operations such as read.
+        Returns:
+        A pandas DataFrame
 
-    Returns:
+        Note:
+        - The function assumes the uploaded CSV has specific columns of interest, notably 'PhoneNo' and 'UserKeyPress'.
+        - It is assumed that the second row of the CSV provides the column names for the data.
+        """
 
-    Note:
-    - The function assumes the uploaded CSV has specific columns of interest, notably 'PhoneNo' and 'UserKeyPress'.
-    - It is assumed that the second row of the CSV provides the column names for the data.
-    """
+        # Keep rows where its respective 'UserKeyPress' column is NA
+        recycled_phonenum = df.loc[df['UserKeyPress'].isna()]
 
-    df = pd.read_csv(uploaded_file, skiprows=1, names=range(100), engine='python')
+        recycled_phonenum = recycled_phonenum[['PhoneNo']]
 
-    df.dropna(axis='columns', how='all', inplace=True)
+        # Rename to follow usual convention
+        recycled_phonenum.rename(columns={'PhoneNo': 'phonenum'}, inplace=True)
 
-    df.columns = df.iloc[0]
+        return recycled_phonenum
+    
+    
+    def calculate_total_cr(self, df) -> int:
 
-    # Keep rows where its respective 'UserKeyPress' column is NA
-    recycled_phonenum = df.loc[df['UserKeyPress'].isna()]
+        # Replace non-keypresses as blanks
+        complete_keypress = df.replace(r'FlowNo_\d{1,2}=$', np.NaN, regex=True)
 
-    recycled_phonenum = recycled_phonenum[['PhoneNo']]
+        # Remove incomplete rows
+        complete_keypress.dropna(inplace=True)
 
-    recycled_phonenum.rename(columns={'PhoneNo': 'phonenum'}, inplace=True)
+        cr_count = len(complete_keypress)
 
-    return recycled_phonenum
+        return cr_count
+
