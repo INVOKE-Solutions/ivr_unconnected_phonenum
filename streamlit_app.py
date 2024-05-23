9381273ee1a9a9e684687eff5fd1df7db33dfc82
judmn <junaidi@invokeisdata.com>
2024-05-23 10:51:09 +0800

    refactored code

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,12 +6,9 @@ from utils import IvrProcessor
 
 st.set_page_config(
     page_title='IVR Unconnected Phone Numbers',
-    # page_icon=Image.open('./images/invoke_logo.png'),
     layout="centered",    
     initial_sidebar_state="auto"
 )
-
-st.title('IVR Unconnected Phone Numbers')
 
 hide_streamlit_style = """
             <style>
@@ -20,7 +17,10 @@ hide_streamlit_style = """
             footer {visibility: hidden;}
             </style>
             """
+
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+st.title('IVR Unconnected Phone Numbers')
 
 st.markdown(
     '''
@@ -28,103 +28,99 @@ st.markdown(
     '''
 )
 
-def main():
-    
-    # Check if data is already processed and available in session state
-    if 'processed' not in st.session_state:
-        st.session_state['processed'] = False
-      
-    if 'all_unconnected_phonenum' not in st.session_state:
+# Initialize session state variables if not already present
+if 'uploaded_files' not in st.session_state:
+    st.session_state['uploaded_files'] = None
+if 'process_complete' not in st.session_state:
+    st.session_state['process_complete'] = False
 
-        # Setting up the file uploader widget
-        uploaded_files = st.file_uploader("Upload IVR Files (.csv format)",
-                                          accept_multiple_files=True, 
-                                          type=['csv', 'xlsx'], 
-                                          help='You can upload multiple csv files at once')
+# Setting up the file uploader widget
+uploaded_files = st.file_uploader("Upload IVR Files (.csv format)",
+                                    accept_multiple_files=True, 
+                                    type=['csv', 'xlsx'], 
+                                    help='You can upload multiple csv files at once')
 
-        # Display the count of uploaded files
-        if uploaded_files:
-            st.write(f"Number of files uploaded: {len(uploaded_files)}")
+# Update the session state with the uploaded file
+if uploaded_files is not None:
+    st.session_state['uploaded_files'] = uploaded_files
+else:
+    st.session_state['uploaded_files'] = False
+    st.session_state['process_complete'] = False
 
-            if st.button('Process'):
-                with st.spinner("Processing the files..."):
+if st.session_state['uploaded_files']:
+    # Display the count of uploaded files
+    st.write(f"Number of files uploaded: {len(uploaded_files)}")
 
-                    unconnected_phonenums = []
-                    total_cr = 0
+    if st.button('Process'):
+        # Process the files
+        with st.spinner("Processing the files..."):
 
-                    # Extract unconnected phone numbers only
-                    for uploaded_file in uploaded_files:
-                        
-                        # Initialize class
-                        ip = IvrProcessor()
+            # Initialize class
+            ip = IvrProcessor()
 
-                        ivr_file = ip.read_file(uploaded_file)
+            unconnected_phonenums = []
+            total_cr = 0
 
-                        unconnected_phonenum = ip.extract_unconnected_phonenum(ivr_file)
+            # Extract unconnected phone numbers only
+            for uploaded_file in uploaded_files:
 
-                        unconnected_phonenums.append(unconnected_phonenum)
+                ivr_file = ip.read_file(uploaded_file)
 
-                        cr_count = ip.calculate_total_cr(ivr_file)
+                unconnected_phonenum = ip.extract_unconnected_phonenum(ivr_file)
+                unconnected_phonenums.append(unconnected_phonenum)
 
-                        total_cr += cr_count
+                cr_count = ip.calculate_total_cr(ivr_file)
+                total_cr += cr_count
 
-                    all_unconnected_phonenum = pd.concat(unconnected_phonenums, ignore_index=True).drop_duplicates()
+                # Store total CR value in session state
+                st.session_state['total_cr'] = total_cr
 
-                    st.session_state['all_unconnected_phonenum'] = all_unconnected_phonenum
-                    
-                    st.session_state['processed'] = True
+            all_unconnected_phonenum = pd.concat(unconnected_phonenums, ignore_index=True).drop_duplicates()
 
-                    # Calculate total CR
+            # Randomize the phone numbers sequence
+            all_unconnected_phonenum = all_unconnected_phonenum.sample(len(all_unconnected_phonenum))
+
+            # Store the output DF in session state
+            st.session_state['all_unconnected_phonenum'] = all_unconnected_phonenum
+
+            st.success("Files have been processed successfully ✨")
+
+            st.write(f"Total CR: {st.session_state['total_cr']:,}")
+
+            st.write(f"Total unconnected phone numbers: {len(st.session_state['all_unconnected_phonenum']):,}")
+
+            # Display a snippet of the cleaned data randomly
+            st.markdown("### Data Preview")
+            st.dataframe(st.session_state['all_unconnected_phonenum'].sample(6))
+
+            # st.session_state['total_cr'] = total_cr
+            st.session_state['process_complete'] = True  # Set process complete flag
+
+# Warning if 'Process' is clicked before file upload
+if not st.session_state['uploaded_files'] and st.button("Process"):
+    st.warning("Please upload your IVR raw files first.")
+
+@st.experimental_fragment
+def download_file(data, file_name):
+    st.download_button(
+        label = "Download file",
+        data = data,
+        file_name = file_name,
+        mime = 'text/csv',
+        key = 'download'
+    )
 
 
-    if st.session_state['processed']:
-        st.success("Files have been processed successfully ✨")
+# Show 'Download' button if the file has been processed
+if st.session_state['uploaded_files'] and st.session_state['process_complete']:
 
-    if st.session_state['processed']:
+    # Generate current date for file versioning
+    formatted_date = datetime.now().strftime("%d%m%Y")
 
-        # Display a snippet of the cleaned data
-        st.markdown("### Data Preview")
+    # Default file name with current date
+    default_filename = f'IVR_unconnected_phonenum_v{formatted_date}.csv'
 
-        st.write(f"Total CR: {total_cr:,}")
+    # Convert to csv
+    data_as_csv = st.session_state['all_unconnected_phonenum'].to_csv(index=False).encode('utf-8')
 
-        st.write(f"Total unconnected phone numbers: {len(st.session_state['all_unconnected_phonenum']):,}")
-
-        st.dataframe(st.session_state['all_unconnected_phonenum'].sample(6))
-
-        # Current date for the filename
-        formatted_date = datetime.now().strftime("%d%m%Y")
-
-        # Format the default filename
-        default_filename = f'IVR_unconnected_phonenum_v{formatted_date}.csv'
-
-        # Use the default filename in the text input, allowing the user to edit it
-        output_filename = st.text_input("Custom the file name before downloading if needed", value=default_filename)
-
-        # Check if the output filename ends with '.csv', if not append '.csv'
-        if not output_filename.lower().endswith('.csv'):
-            output_filename += '.csv'
-    
-        # Download unconnected phone numbers list
-        if st.session_state['processed']:
-            
-            # Randomize the phone numbers
-            all_unconnected_phonenum = st.session_state['all_unconnected_phonenum'].sample(len(st.session_state['all_unconnected_phonenum']))
-
-            # Convert to csv
-            data_as_csv = all_unconnected_phonenum.to_csv(index=False).encode('utf-8')
-
-            st.download_button(
-                label="Download file",
-                data=data_as_csv,
-                file_name=output_filename,
-                mime='text/csv',
-                key='download'
-            )
-        
-        if 'downloaded' in st.session_state:
-            st.write('**\*Reload the page to upload another IVR results.**')
-        else:
-            st.session_state['downloaded'] = True
-        
-if __name__ == '__main__':
-    main()
+    download_file(data_as_csv, default_filename)              
