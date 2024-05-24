7ff7b7d31517cdc0203a3d33b846298bb0c910f0
judmn <junaidi@invokeisdata.com>
2024-05-24 17:02:15 +0800

    add a bit more padding at the bottom

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ st.markdown("""
             <style>
             .block-container {
                     padding-top: 0rem;
-                    padding-bottom: 0rem;
+                    padding-bottom: 2rem;
                     padding-left: 2rem;
                     padding-right: 2rem;
                 }
