fb4a338863d08951295a45718173d9ce7732ff25
judmn <junaidi@invokeisdata.com>
2024-06-23 14:08:54 +0800

    edit app's description

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,9 +35,7 @@ st.title('IVR Unconnected Phone Numbers')
 
 st.markdown(
     '''
-    This app removes already participated phone numbers from IVR raw results and churn out the ones who haven't to be re-used in the next round of IVR.  
-
-    **:red[NOTE:]** This web app is meant to be used when all phone numbers for a given population is included in the sampling.
+    This app removes already participated phone numbers from IVR raw results and churn out the ones who haven't to be re-used in the next round of IVR.
     '''
 )
 
